e4ba8a97bb5295d1160b91e882dca239373df382
suyashshukla <suyashshukla1997@gmail.com>
2019-12-20 10:02:25 +0530

    Some Tricky Inheritance in C#

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace BonjourBasics
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            Car car = new MiniCar();
+            // MiniCar miniCar = new RaceCar();
+            //RaceCar raceCar = new RaceCar();
+
+            //Console.WriteLine(car.Speed());
+            //Console.WriteLine(miniCar.Speed());
+            //Console.WriteLine(raceCar.Speed());
+
+            Console.ReadLine();
+
+        }
+    }
+
+    internal class Car
+    {
+        public Car()
+        {
+            Console.WriteLine("Car");
+        }
+
+        public int Speed()
+        {
+            return 0;
+        }
+    }
+
+    internal class MiniCar : Car
+    {
+
+        public MiniCar()
+        {
+            Console.WriteLine("MiniCar");
+        }
+
+        public new int Speed()
+        {
+            return 60;
+        }
+    }
+
+    internal class RaceCar : MiniCar
+    {
+        public RaceCar()
+        {
+            Console.WriteLine("RaceCar");
+        }
+
+        public new int Speed()
+        {
+            return 150;
+        }
+
+        public string Power()
+        {
+            return "750CC";
+        }
+    }
+
+}
